69fa1e5aa4d1e2a77b33af175240d47cbaad7410
Gabriel Pagola <gabrielpagola17@gmail.com>
2024-04-20 09:23:58 +0200

    refactor reverseString in order to pass the tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(data) {
+  if(typeof data !== 'string') {
+    return false;
+  }
 
+  let reverse = '';
+
+  for(let i = data.length-1; i >= 0; i--) {
+    reverse += data.charAt(i);
+  }
+
+  return reverse;
 };
 
 // Do not edit below this line
