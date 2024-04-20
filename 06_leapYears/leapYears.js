69497c1fd45c3581235841e8709965ba8210144a
Gabriel Pagola <gabrielpagola17@gmail.com>
2024-04-20 18:41:47 +0200

    refactor leapYear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+  let isLeapYear = false;
+
+  if(year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    isLeapYear = true;
+  }
+  
+  return isLeapYear;
 
 };
 
