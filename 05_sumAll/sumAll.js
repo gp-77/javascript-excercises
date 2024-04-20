8358b92a719b753419c04636b08a9ff3e58c3113
Gabriel Pagola <gabrielpagola17@gmail.com>
2024-04-20 14:22:54 +0200

    refactor sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
 
+  if(typeof start !== 'number' || typeof stop !== 'number') {
+    return 'ERROR';
+  }
+
+  if(start < 0 || stop < 0) {
+    return 'ERROR';
+  }
+
+  if(stop < start) {
+    let tempNr = start;
+    start = stop;
+    stop = tempNr;
+  }
+
+  let sum = 0;
+
+  for(let i=start; i<=stop; i++ ) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
