3121f7799f2413a36742b93b83bcacd42535b67a
Gabriel Pagola <gabrielpagola17@gmail.com>
2024-04-20 13:56:05 +0200

    refactor removeFromArray function to pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,35 @@
-const removeFromArray = function() {
+const removeFromArray = function(data, ...parameters) {
 
+  if(!Array.isArray(data)) {
+    return 'ERROR';
+  }
+
+  if(data.length === 0) {
+    return [];
+  }
+
+  // let index = -1;
+
+  // for(let param of parameters) {
+  //   index = data.indexOf(param);
+
+  //   if(index !== -1) {
+  //     data.splice(index,1);
+  //   }
+  // }
+
+  const newArray = [];
+
+  data.forEach((item) => {
+    // push every element into the new array
+    // UNLESS it is included in the function arguments
+    // so we create a new array with every item, except those that should be removed
+    if (!parameters.includes(item)) {
+      newArray.push(item);
+    }
+  });
+
+  return newArray;
 };
 
 // Do not edit below this line
