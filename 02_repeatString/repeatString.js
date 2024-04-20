26a4442705e79a2e67c4ae6e2b0f9a1256917a54
Gabriel Pagola <gabrielpagola17@gmail.com>
2024-04-20 05:45:44 +0200

    repeatString function refactor to pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function( data, repetition) {
 
+    if(!Number.isInteger(repetition) ||  repetition < 0) {
+        return "ERROR";
+    }
+
+    let result = '';
+    for(let i=0; i<repetition; i++) {
+        result = result + data;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
