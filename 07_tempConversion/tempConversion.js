781810e496a330b0b94bc1af7291f659001e9c5d
Gabriel Pagola <gabrielpagola17@gmail.com>
2024-04-20 19:10:24 +0200

    refactor conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celsius =  (fahrenheit - 32) * (5/9);
+
+  celsius = celsius.toFixed(1);
+  celsius = parseFloat(celsius);
+  if(celsius % 10 === 0) {
+    celsius = parseInt(celsius);
+  }
+ 
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  fahrenheit = (celsius * (9/5)) + 32;
+  fahrenheit = fahrenheit.toFixed(1);
+  
+  return parseFloat(fahrenheit);
 };
 
 // Do not edit below this line
