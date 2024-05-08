35fe911bb82097fd0b0a2f19ac405d2bf69fa814
Gabriel Pagola <gabrielpagola17@gmail.com>
2024-05-09 01:07:02 +0200

    add calculator logic

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total * item);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number === 0) return 1;
+  let factorial = 0;
+	if(number > 0) {
+    for(let i=0; i<=number; i++) {
+      if(i===0) {
+        factorial = 1;
+        continue;
+      }
+      factorial = factorial * i;
+    }
+  }
+  return factorial;
 };
 
 // Do not edit below this line
