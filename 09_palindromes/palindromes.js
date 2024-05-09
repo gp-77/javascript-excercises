1896d671949da0e451c910677ad452d55df9ee3c
Gabriel Pagola <gabrielpagola17@gmail.com>
2024-05-09 14:18:28 +0200

    add function to check if a string is palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,38 @@
-const palindromes = function () {
+// const palindromes = function (string) {
 
-};
+//   let isPalindrome = false;
+
+//   let cleanString = string.replace(/[^\w\s]|_/g, "").replace(/\s+/g, '');
+
+//   cleanStr = cleanString.toLowerCase();
+
+//   let newString = '';
+//   for(let i=cleanStr.length - 1; i>=0; i--) {
+//     newString += cleanStr[i];
+//   }
+
+//   if(newString === cleanStr) isPalindrome = true;
+
+//   return isPalindrome;
+// };
+
+
+const palindromes = function (string) {
+  let isPalindrome = false;
+
+  let cleanString = string.replace(/[^\w\s]|_/g, "").replace(/\s+/g, '');
+
+  let cleanStr = cleanString.toLowerCase();
+
+  let arrString = cleanStr.split("");
+  let arrReverseString = arrString.reverse();
+  let reverseString = arrReverseString.join("");
+
+  if(reverseString === cleanStr) isPalindrome = true;
+
+  return isPalindrome;
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
