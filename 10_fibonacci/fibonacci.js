bb152459eb5ab4a943cba054562b5458410cfea4
Gabriel Pagola <gabrielpagola17@gmail.com>
2024-05-09 14:43:35 +0200

    add fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+  if(typeof number !== 'number') {
+    number = parseInt(number);
+  }
+  
+  if(number < 0) {
+    return "OOPS";
+  } else if(number === 0) {
+    return 0;
+  }
+
+  let firstPrev = 1;
+  let secondPrev = 0;
+
+  for(i=2; i<=number; i++) {
+    let current = firstPrev + secondPrev;
+    secondPrev = firstPrev;
+    firstPrev = current;
+  }
+
+  return firstPrev;
 
 };
 
