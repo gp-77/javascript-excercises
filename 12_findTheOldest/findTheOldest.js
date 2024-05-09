916030ae172c4aa3d012e220d0869367cd8014c6
Gabriel Pagola <gabrielpagola17@gmail.com>
2024-05-09 23:53:54 +0200

    add function findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let oldest = [];
 
+  people.forEach(person => {
+    if(person.hasOwnProperty('yearOfDeath') === false) {
+      const d = new Date();
+      person.yearOfDeath = parseInt(d.getFullYear());
+    }
+    console.log(person);
+
+    if(oldest.length === 0) {
+      oldest = person;
+    } else {
+      let gap = person.yearOfDeath - person.yearOfBirth;
+      let gapOldest = oldest.yearOfDeath - oldest.yearOfBirth;
+      console.log(gap);
+
+      if(gapOldest < gap) oldest = person;
+    }
+  });
+
+  return oldest;
 };
 
 // Do not edit below this line
